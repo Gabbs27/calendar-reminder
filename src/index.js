4dd0fafd8a6bc2fcad2a9dea4cb6c99e55a994d2
Gabriel <djabreu_@hotmail.com>
2023-01-27 18:19:23 -0400

    Limpiando los archivos

--- src/index.js
+++ src/index.js
@@ -4,25 +4,30 @@ import { Provider as ReduxProvider } from "react-redux";
 import "normalize.css";
 import "./styles/index.scss";
 
-import Main from "./Main";
-import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 import { store } from "./store/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./pages/App";
+import Calendar from "./pages/Calendar";
 
 // import main sass file
 //import "./sass/app.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ReduxProvider store={store}>
-      <Main />
-    </ReduxProvider>
-  </React.StrictMode>,
+  <ReduxProvider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<App />} />
+        <Route exact path='/calendar' element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
+  </ReduxProvider>,
   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
 serviceWorker.unregister();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import App from "../pages/App";
-import Calendar from "../pages/Calendar";
-
 function routes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route exact path="/calendar" element={<Calendar />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <></>;
 }
 
 export default routes;
